035f45e311cc8628d233788d675dd216fa7160fb
Andy Ta <andyta.2610@gmail.com>
2026-03-09 19:52:59 +0700

    fix news data

--- news.py
+++ news.py
@@ -3,28 +3,28 @@ news_data = [
 {
 "id":1,
 "title":"Một ngày ở Bình Đại Bến Tre",
-"date":"2025",
+"year":"2025",
 "source":"Kiến Việt",
-"url":"https://kienviet.net/2025/5/3/tan-man-duoi-mai-ngoi-do-mot-ngay-binh-yen-o-casa-blanca-garden-h2/",
-"thumbnail":"/static/news/1.jpg"
+"link":"https://kienviet.net/2025/5/3/tan-man-duoi-mai-ngoi-do-mot-ngay-binh-yen-o-casa-blanca-garden-h2/",
+"image":"/static/news/1.jpg"
 },
 
 {
 "id":2,
 "title":"Top 10 công trình của năm",
-"date":"2026",
+"year":"2026",
 "source":"Top10awards",
-"url":"https://top10awards.vn/casa-blanca-garden-h-2-workshop/",
-"thumbnail":"/static/news/2.jpg"
+"link":"https://top10awards.vn/casa-blanca-garden-h-2-workshop/",
+"image":"/static/news/1.jpg"
 },
 
 {
 "id":3,
 "title":"Nhà Bến Tre trên tạp chí kiến trúc Mỹ",
-"date":"2025",
+"year":"2025",
 "source":"Archdaily",
-"url":"https://www.archdaily.com/1028385/casa-blanca-garden-h2",
-"thumbnail":"/static/news/3.jpg"
+"link":"https://www.archdaily.com/1028385/casa-blanca-garden-h2",
+"image":"/static/news/3.jpg"
 }
 
 ]
