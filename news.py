e8ed770048f7bfcc5303c2bb93d2def1b6a2ca61
Andy Ta <andyta.2610@gmail.com>
2026-03-09 19:48:34 +0700

    load news css

--- news.py
+++ news.py
@@ -4,7 +4,7 @@ news_data = [
 "id":1,
 "title":"Một ngày ở Bình Đại Bến Tre",
 "date":"2025",
-"source":"ArchDaily",
+"source":"Kiến Việt",
 "url":"https://kienviet.net/2025/5/3/tan-man-duoi-mai-ngoi-do-mot-ngay-binh-yen-o-casa-blanca-garden-h2/",
 "thumbnail":"/static/news/1.jpg"
 },
@@ -12,8 +12,8 @@ news_data = [
 {
 "id":2,
 "title":"Top 10 công trình của năm",
-"date":"2024",
-"source":"Dezeen",
+"date":"2026",
+"source":"Top10awards",
 "url":"https://top10awards.vn/casa-blanca-garden-h-2-workshop/",
 "thumbnail":"/static/news/2.jpg"
 },
@@ -21,8 +21,8 @@ news_data = [
 {
 "id":3,
 "title":"Nhà Bến Tre trên tạp chí kiến trúc Mỹ",
-"date":"2024",
-"source":"DesignBoom",
+"date":"2025",
+"source":"Archdaily",
 "url":"https://www.archdaily.com/1028385/casa-blanca-garden-h2",
 "thumbnail":"/static/news/3.jpg"
 }
